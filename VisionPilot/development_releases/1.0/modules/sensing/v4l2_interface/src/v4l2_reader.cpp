8e80d374ee1140498a761dfe0f3f320272d946d9
TranHuuNhatHuy <huy9515@gmail.com>
2026-05-11 10:40:40 +0900

    init the v4l2 implementation

--- VisionPilot/development_releases/1.0/modules/sensing/v4l2_interface/src/v4l2_reader.cpp
+++ VisionPilot/development_releases/1.0/modules/sensing/v4l2_interface/src/v4l2_reader.cpp
@@ -1,2 +1,24 @@
 #include <v4l2_interface/v4l2_reader.hpp>
 
+#include <sstream>
+#include <iomanip>
+#include <chrono>
+#include <thread>
+
+
+namespace v4l2_interface {
+
+    V4L2Reader::V4L2Reader(
+        const std::string& device_path,
+        uint32_t fps
+    ) : device_path(device_path),
+        target_fps(fps),
+        is_stream_started(false),
+        has_latest_frame(false)
+    {
+        
+        log_info("Initializing V4L2 Reader");
+        log_info("  Device Path: " + device_path);
+        log_info("  Target FPS: " + std::to_string(fps));
+
+}
